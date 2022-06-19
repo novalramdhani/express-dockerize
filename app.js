0d7e9bea38dc74aa5beeec8c3c531db78924e9ac
novalramdhani <novalfitrahramdhani@gmail.com>
2022-06-19 23:13:53 +0700

    refactoring

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-    res.send("Hello Express!!!");
+    res.status(200)
+        .json({
+            message: "Hello Express"
+        });
 });
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}, http:localhost:${port}`);
 });
+
+module.exports = app;
